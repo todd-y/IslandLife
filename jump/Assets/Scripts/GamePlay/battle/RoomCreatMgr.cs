9bfd49c94582012fe881f235f25a18d8e5629f71
todd.y <723484094@qq.com>
2017-01-10 11:26:32 +0800

    jump

--- jump/Assets/Scripts/GamePlay/battle/RoomCreatMgr.cs
+++ jump/Assets/Scripts/GamePlay/battle/RoomCreatMgr.cs
@@ -38,15 +38,30 @@ public class RoomCreatMgr : Singleton<RoomCreatMgr> {
 
         while (creatY < yCount) {
             int creatType = Random.Range(0, 100);
-            if (creatType < 10) {
-                GridType[][] arrData = GetPlatform(Random.Range(0f, 1f) >= 0.5f);
+            if (creatType < 5) {
+                GridType[][] arrData = GetPlatform(PlatformType.Left);
                 AddData(creatY, arrData);
-                creatY += (arrData.Length  + 1);
+                creatY += (arrData.Length + Random.Range(1, 4));
             }
-            else if (creatType < 50) {
+            else if (creatType < 10) {
+                GridType[][] arrData = GetPlatform(PlatformType.Right);
+                AddData(creatY, arrData);
+                creatY += (arrData.Length + Random.Range(1, 4));
+            }
+            else if (creatType < 15) {
+                GridType[][] arrData = GetRulePlatform(PlatformType.Double);
+                AddData(creatY, arrData);
+                creatY += (arrData.Length + Random.Range(1, 4));
+            }
+            else if (creatType < 30) {
+                GridType[][] arrData = GetSquare();
+                AddData(creatY, arrData);
+                creatY += (arrData.Length + Random.Range(1, 3));
+            }
+            else if (creatType < 70) {
                 GridType[][] arrData = GetLine();
                 AddData(creatY, arrData);
-                creatY += arrData.Length;
+                creatY += (arrData.Length + Random.Range(1, 3));
             }
             else {
                 creatY += Random.Range(1, 5);
@@ -81,13 +96,13 @@ public class RoomCreatMgr : Singleton<RoomCreatMgr> {
         }
     }
 
-    private GridType[][] GetLine(int xMin = 0, int xMax = -1, int numMin = 2, int numMax = 4) {
-        if (xMax == -1) {
-            xMax = xCount;
+    private GridType[][] GetLine(int xMinStart = 0, int xMaxStart = -1, int numMin = 1, int numMax = 7) {
+        if (xMaxStart == -1) {
+            xMaxStart = xCount;
         }
         GridType[][] arrLine = new GridType[1][];
         int num = Random.Range(numMin, numMax);
-        int startX = Random.Range(xMin, xMax - num + 1);
+        int startX = Random.Range(xMinStart, xMaxStart + 1 - num);
         arrLine[0] = new GridType[xCount];
         for (int x = 0; x < num; x++) {
             arrLine[0][startX + x] = GridType.Wall;
@@ -96,12 +111,12 @@ public class RoomCreatMgr : Singleton<RoomCreatMgr> {
         return arrLine;
     }
 
-    private GridType[][] GetPlatform(bool leftStart, int minY = 1, int maxY = 6, int minX = 1, int maxX = 8) {
+    private GridType[][] GetPlatform(PlatformType type, int minX = 1, int maxX = 8, int minY = 2, int maxY = 6) {
         int numY = Random.Range(minY, maxY + 1);
 
         GridType[][] arrData = new GridType[numY][];
         int longestY = Random.Range(0, numY);
-        int longestX = Random.Range( Mathf.Max(numY, minX), maxX + 1);
+        int longestX = Random.Range(minX, maxX + 1);
 
         int[] arrNumX = new int[numY];
         arrNumX[longestY] = longestX;
@@ -125,12 +140,86 @@ public class RoomCreatMgr : Singleton<RoomCreatMgr> {
             int numX = arrNumX[y];
             arrData[y] = new GridType[xCount];
             for (int x = 0; x < numX; x++) {
-                if (leftStart) {
-                    arrData[y][x] = GridType.Wall;
+                switch(type){
+                    case PlatformType.Left:
+                        arrData[y][x] = GridType.Wall;
+                        break;
+                    case PlatformType.Right:
+                        arrData[y][xCount - 1 - x] = GridType.Wall;
+                        break;
+                    case PlatformType.Double:
+                        arrData[y][x] = GridType.Wall;
+                        arrData[y][xCount - 1 - x] = GridType.Wall;
+                        break;
                 }
-                else {
-                    arrData[y][xCount - 1 - x] = GridType.Wall;
+            }
+        }
+
+        return arrData;
+    }
+
+    private GridType[][] GetRulePlatform(PlatformType type, int minX = 3, int maxX = 5, int minY = 3, int maxY = 6) {
+        int numY = Random.Range(minY, maxY + 1);
+
+        GridType[][] arrData = new GridType[numY][];
+        int longestY = Random.Range(0, numY);
+        int longestX = Random.Range(Mathf.Max(longestY, minY), maxX + 1);
+
+        for (int y = 0; y < numY; y++) {
+            int numX = longestX - Mathf.Abs(longestY - y);
+            arrData[y] = new GridType[xCount];
+            for (int x = 0; x < numX; x++) {
+                switch (type) {
+                    case PlatformType.Left:
+                        arrData[y][x] = GridType.Wall;
+                        break;
+                    case PlatformType.Right:
+                        arrData[y][xCount - 1 - x] = GridType.Wall;
+                        break;
+                    case PlatformType.Double:
+                        arrData[y][x] = GridType.Wall;
+                        arrData[y][xCount - 1 - x] = GridType.Wall;
+                        break;
                 }
+            }
+        }
+
+        return arrData;
+    }
+
+    private GridType[][] GetSquare(int startMinX = 1, int startMaxX = 12, int minY = 2, int maxY = 4, int minX = 3, int maxX = 6) {
+        int numY = Random.Range(minY, maxY + 1);
+
+        GridType[][] arrData = new GridType[numY][];
+        int longestY = Random.Range(0, numY);
+        int longestX = Random.Range(minX, maxX + 1);
+
+        int[] arrNumX = new int[numY];
+        arrNumX[longestY] = longestX;
+        int reduceY = longestY - 1;
+        int reduceMaxX = longestX;
+        while (reduceY >= 0) {
+            reduceMaxX = Random.Range(Mathf.Max(1, reduceMaxX - 3), reduceMaxX + 1);
+            arrNumX[reduceY] = reduceMaxX;
+            reduceY--;
+        }
+
+        reduceY = longestY + 1;
+        reduceMaxX = longestX;
+        while (reduceY < numY) {
+            reduceMaxX = Random.Range(Mathf.Max(1, reduceMaxX - 3), reduceMaxX + 1);
+            arrNumX[reduceY] = reduceMaxX;
+            reduceY++;
+        }
+
+        int startX = Random.Range(startMinX, startMaxX + 1 - longestX);
+        for (int y = 0; y < numY; y++ ) {
+            int numX = arrNumX[y];
+            arrData[y] = new GridType[xCount];
+            int deltaX = longestX - numX;
+            int fixX = deltaX % 2 == 0 ? deltaX / 2 : ((int)deltaX / 2 + Random.Range(0, 2));
+            for (int x = 0; x < numX; x++ ) {
+                arrData[y][startX + fixX + x] = GridType.Wall;
             }
         }
 
@@ -215,5 +304,11 @@ public class RoomCreatMgr : Singleton<RoomCreatMgr> {
         }
         go.SetActive(false);
         go.transform.SetParent(poolGo.transform, false);
+    }
+
+    public enum PlatformType {
+        Left,
+        Right,
+        Double,
     }
 }
